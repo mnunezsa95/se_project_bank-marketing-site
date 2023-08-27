eaedf202ea09142698281cd0d606365d42764af3
mnunezsa <mnunezsa95@gmail.com>
2023-08-27 19:04:43 -0400

    add smooth scrolling

---
+++ constants.js
@@ -0,0 +1,12 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+const nav = document.querySelector(".nav");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+export { modal, overlay, btnCloseModal, btnsOpenModal, btnScrollTo, section1, nav, tabs, tabsContainer, tabsContent };
--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-'use strict';
-
-///////////////////////////////////////
-// Modal window
-
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+"use strict";
+import { modal, overlay, btnCloseModal, btnsOpenModal, btnScrollTo, section1 } from "./constants.js";
 
 const openModal = function (evt) {
   evt.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
-btnsOpenModal.forEach(btn => {
-  btn.addEventListener('click', openModal);
+btnsOpenModal.forEach((btn) => {
+  btn.addEventListener("click", openModal);
 });
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
+});
+
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  //Scrolling Modern
+  section1.scrollIntoView({ behavior: "smooth" });
 });
